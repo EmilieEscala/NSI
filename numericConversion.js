12c32959d4c0662114e9934058e19654f8d2a8eb
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-02 17:59:02 +0100

    Add files via upload

---
+++ numericConversion.js
@@ -0,0 +1,7 @@
+function hexDigitToBin(hexDigit){
+	//Compléter cette fonction de conversion d'un caractère représentant un chiffre hexadécimal
+	//retournant le mot binaire correspondant.
+	upperCaseHexDigit = hexDigit.toUpperCase()
+	return ;
+}
+
