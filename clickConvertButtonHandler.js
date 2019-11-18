9edda79a9d1eccc0e02dd0056eaea7552f51faed
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-18 11:06:47 +0100

    Update clickConvertButtonHandler.js

--- clickConvertButtonHandler.js
+++ clickConvertButtonHandler.js
@@ -5,3 +5,8 @@ function main(){
 	convertButton.onclick = numericConversion
 }
 
+function displayHexToBin(binWord){
+	// Affiche la chaîne de caractère myText donnée en paramètre dans le
+	//paragraphe HTML d'identifiant display.
+	hexToBinPlaceHolder.textContent = binWord
+}
