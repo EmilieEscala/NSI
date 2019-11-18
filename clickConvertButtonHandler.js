b27c0aa22412af11349392375261b5c48211764c
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-18 11:07:42 +0100

    Update clickConvertButtonHandler.js

--- clickConvertButtonHandler.js
+++ clickConvertButtonHandler.js
@@ -2,8 +2,22 @@ function main(){
 	// Fonction executée après que la page web a été totalement chargée.
 	// Si on l'executait avant le chargement de l'élément
 	//html convertButton, l'événement de click de pourrait y êre associé.
-	convertButton.onclick = numericConversion
+	convertButton.onclick = convertionHandler
 }
+
+function convertionHandler(){
+	// La fonction proceed réalise la conversion de la chaîne entrée dans
+	//hexInput et affiche le résultat dans display.
+
+	// Version utilisant des variables intermediaires
+	//hexString = getHexString();
+	//binWord = hexWordToBin(hexString);
+	//displayHexToBin(binWord);
+
+	//version utilisant la composition de fonction
+	displayHexToBin(hexWordToBin(getHexString()))
+}
+
 
 function displayHexToBin(binWord){
 	// Affiche la chaîne de caractère myText donnée en paramètre dans le
