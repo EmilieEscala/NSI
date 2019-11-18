24a3680f6504de2fc6d49662873580c113e2f274
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-18 11:05:14 +0100

    Update clickConvertButtonHandler.js

--- clickConvertButtonHandler.js
+++ clickConvertButtonHandler.js
@@ -2,6 +2,6 @@ function main(){
 	// Fonction executée après que la page web a été totalement chargée.
 	// Si on l'executait avant le chargement de l'élément
 	//html convertButton, l'événement de click de pourrait y êre associé.
-	convertButton.onclick = numericConversion.js
+	convertButton.onclick = numericConversion
 }
 
