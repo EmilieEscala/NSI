65e842990e7ed99cd1d9c7518ba65336098bfc20
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-18 10:30:24 +0100

    Update numericConversion.js

--- numericConversion.js
+++ numericConversion.js
@@ -5,3 +5,21 @@ function hexDigitToBin(hexDigit){
 	return ;
 }
 
+function convertionHandler(){
+	// La fonction proceed réalise la conversion de la chaîne entrée dans
+	//hexInput et affiche le résultat dans display.
+
+	// Version utilisant des variables intermediaires
+	//hexString = getHexString();
+	//binWord = hexWordToBin(hexString);
+	//displayHexToBin(binWord);
+
+	//version utilisant la composition de fonction
+	displayHexToBin(hexWordToBin(getHexString()))
+}
+
+function getHexString(){
+	//getHexString récupère la chaine de caractère entrée dans le champ
+	//hexInput
+	return hexInput.value;
+}
