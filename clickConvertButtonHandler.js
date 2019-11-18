294ab8e35326ed4e3598d8082ce0d271590b8cfb
EmilieEscala <57296769+EmilieEscala@users.noreply.github.com>
2019-11-18 10:29:55 +0100

    Update clickConvertButtonHandler.js

--- clickConvertButtonHandler.js
+++ clickConvertButtonHandler.js
@@ -5,25 +5,6 @@ function main(){
 	convertButton.onclick = convertionHandler
 }
 
-function convertionHandler(){
-	// La fonction proceed réalise la conversion de la chaîne entrée dans
-	//hexInput et affiche le résultat dans display.
-
-	// Version utilisant des variables intermediaires
-	//hexString = getHexString();
-	//binWord = hexWordToBin(hexString);
-	//displayHexToBin(binWord);
-
-	//version utilisant la composition de fonction
-	displayHexToBin(hexWordToBin(getHexString()))
-}
-
-function getHexString(){
-	//getHexString récupère la chaine de caractère entrée dans le champ
-	//hexInput
-	return hexInput.value;
-}
-
 function displayHexToBin(binWord){
 	// Affiche la chaîne de caractère myText donnée en paramètre dans le
 	//paragraphe HTML d'identifiant display.
